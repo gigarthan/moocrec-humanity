3b66b60f49b1c6d2290ba95e30e825d3d866f039
gigarthan <gigarthan@parkwaylabs.com>
2019-06-23 08:19:32 +0530

    setup homepage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,25 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 export default function HomePage() {
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <section className="flex bg-white">
+      <section className="w-full h-screen">
+        <div className="flex h-full items-center content-center justify-center">
+          <div>
+            <h1 className="text-4xl font-bold">
+              Find your style; Find your course
+            </h1>
+            <button
+              type="button"
+              className="bg-black text-white p-4 text-2xl rounded-full w-4/5 mt-2 ml-10 tracking-wider self-center"
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+      </section>
+    </section>
   );
 }
